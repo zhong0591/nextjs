52304e285b0185bee318cbcae833b1f1f7f6771c
Seth <zlj@shinetechchina.com>
2026-01-14 09:44:19 +0800

    update main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       <div className="w-full max-w-md space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            登录到您的账户(来自main分支的提示)
+            登录到您的账户(===来自main分支的提示====)
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             请输入您的用户名和密码
